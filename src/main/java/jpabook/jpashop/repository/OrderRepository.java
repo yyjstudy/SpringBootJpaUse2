729f0a7d03789be0926ae6f251969785f8b149f0
yongjo <yjlee90@suresofttech.com>
2022-07-12 13:40:36 +0900

    API 개발 고급 - 지연 로딩과 조회 성능 최적화-간단한 주문 조회 V4: JPA에서 DTO로 바로 조회

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -90,7 +90,6 @@ public class OrderRepository {
         return query.getResultList();
     }
 
-
     public List<Order> findAllWithMemberDelivery() {
         return em.createQuery(
                 "select o from Order o" +
